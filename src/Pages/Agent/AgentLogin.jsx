dfa023f9e71537b84412f1a33607e1b591be4ff3
ikennaex <ikennaexcel@gmail.com>
2026-01-01 11:25:26 +0100

    update: agent login

--- src/Pages/Agent/AgentLogin.jsx
+++ src/Pages/Agent/AgentLogin.jsx
@@ -88,7 +88,7 @@ const AgentLogin = () => {
               onClick={() => setShowPassword(!showPassword)}
               className="absolute right-3 top-1/2 -translate-y-1/2 text-slate-400 hover:text-slate-700"
             >
-              {showPassword ? <EyeOff size={18} /> : <Eye size={18} />}
+              {showPassword ? <Eye size={18} /> : <EyeOff size={18} />}
             </button>
           </div>
 
